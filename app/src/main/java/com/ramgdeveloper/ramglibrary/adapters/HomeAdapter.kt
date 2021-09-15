2c676051aad4768247ce2fec2870f2a04b9e6d30
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-09-15 05:11:09 +0300

    Added Listener To Categories

--- app/src/main/java/com/ramgdeveloper/ramglibrary/adapters/HomeAdapter.kt
+++ app/src/main/java/com/ramgdeveloper/ramglibrary/adapters/HomeAdapter.kt
@@ -1,15 +1,21 @@
 package com.ramgdeveloper.ramglibrary.adapters
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
+import android.view.View.INVISIBLE
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.ramgdeveloper.ramglibrary.data.Category
 import com.ramgdeveloper.ramglibrary.databinding.HomeRowBinding
 
-class HomeAdapter : ListAdapter<Category, HomeAdapter.MyViewHolder>(MyDiffUtil){
+class HomeAdapter(private val onClickListener: OnClickListener) : ListAdapter<Category, HomeAdapter.MyViewHolder>(MyDiffUtil){
     object MyDiffUtil: DiffUtil.ItemCallback<Category>() {
         override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
             return oldItem == newItem
@@ -23,7 +29,29 @@ class HomeAdapter : ListAdapter<Category, HomeAdapter.MyViewHolder>(MyDiffUtil){
             binding.categoryNameTv.text = categories?.categoryName
             Glide.with(binding.categoryImageView)
                 .load(categories?.categoryImage)
-                .centerCrop()
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = INVISIBLE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = INVISIBLE
+                        return false
+                    }
+                })
+                .fitCenter()
                 .into(binding.categoryImageView)
         }
     }
@@ -32,6 +60,13 @@ class HomeAdapter : ListAdapter<Category, HomeAdapter.MyViewHolder>(MyDiffUtil){
     }
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val categories = getItem(position)
+        holder.itemView.setOnClickListener {
+            onClickListener.onClick(categories)
+        }
         holder.bind(categories)
+    }
+
+    class OnClickListener(val clickListener: (category: Category)-> Unit){
+        fun onClick(category: Category) = clickListener(category)
     }
 }
--- app/src/main/java/com/ramgdeveloper/ramglibrary/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/ramgdeveloper/ramglibrary/fragments/home/HomeFragment.kt
@@ -1,42 +1,42 @@
 package com.ramgdeveloper.ramglibrary.fragments.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
-import androidx.navigation.fragment.findNavController
-import com.ramgdeveloper.ramglibrary.R
-import com.ramgdeveloper.ramglibrary.databinding.FragmentHomeBinding
-import android.content.Intent
-import android.net.Uri
-import android.util.Log
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
+import com.ramgdeveloper.ramglibrary.R
 import com.ramgdeveloper.ramglibrary.adapters.HomeAdapter
 import com.ramgdeveloper.ramglibrary.data.Category
+import com.ramgdeveloper.ramglibrary.databinding.FragmentHomeBinding
 
 private const val TAG = "HomeFragment"
+
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var databaseReference: DatabaseReference
-    private lateinit var storageReference: StorageReference
     private lateinit var categoryList: ArrayList<Category>
-    private val adapter by lazy{HomeAdapter()}
+    private lateinit var adapter: HomeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        adapter = HomeAdapter(HomeAdapter.OnClickListener {
+            Toast.makeText(requireContext(), "Happy Study Time", Toast.LENGTH_SHORT).show()
+        })
         loadCategories()
-       // storageReference = FirebaseStorage.getInstance().getReference("images")
+        // storageReference = FirebaseStorage.getInstance().getReference("images")
         binding.fabAdd.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_addBooksFragment)
         }
@@ -46,38 +46,43 @@ class HomeFragment : Fragment() {
         }
         return binding.root
     }
+
     // Show OptionsMenu
-    fun showOptionsMenu(v : View){
+    private fun showOptionsMenu(v: View) {
         val popup = PopupMenu(requireContext(), v)
         val inflater: MenuInflater = popup.menuInflater
         inflater.inflate(R.menu.options_menu, popup.menu)
         popup.setOnMenuItemClickListener { menuItem ->
-            when(menuItem.itemId){
-                R.id.share-> {
+            when (menuItem.itemId) {
+                R.id.share -> {
                     val intent = Intent(Intent.ACTION_SEND)
                     intent.type = "text/plain"
                     intent.putExtra(Intent.EXTRA_SUBJECT, "My application name")
-                    intent.putExtra(Intent.EXTRA_TEXT, "Download my RamgLibrary app https://drive.google.com/file/d/1KS-5NbeDhrN0WPUKr1XzlfOCX1SeIGGK/view?usp=sharing")
+                    intent.putExtra(
+                        Intent.EXTRA_TEXT,
+                        "Download my RamgLibrary app https://drive.google.com/file/d/1KS-5NbeDhrN0WPUKr1XzlfOCX1SeIGGK/view?usp=sharing"
+                    )
                     startActivity(Intent.createChooser(intent, "Share App Via..."))
                 }
-                R.id.about-> {
+                R.id.about -> {
                     val url = "https://github.com/Collince-Okeyo"
                     val intent = Intent(Intent.ACTION_VIEW)
                     intent.data = Uri.parse(url)
                     startActivity(intent)
                 }
-                R.id.help-> {
-                   /* val intent = Intent(Intent.ACTION_VIEW)
-                        .setType("plain/text")
-                        .setData(Uri.parse("collinceokeyo98@gmail.com"))
-                        .setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail")
-                        .putExtra(Intent.EXTRA_SUBJECT, "Help")
-                        .putExtra(Intent.EXTRA_TEXT, "Hello RamG developer....Please help me on how to use the application.");
-                         startActivity(intent)*/
+                R.id.help -> {
+                    /* val intent = Intent(Intent.ACTION_VIEW)
+                         .setType("plain/text")
+                         .setData(Uri.parse("collinceokeyo98@gmail.com"))
+                         .setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail")
+                         .putExtra(Intent.EXTRA_SUBJECT, "Help")
+                         .putExtra(Intent.EXTRA_TEXT, "Hello RamG developer....Please help me on how to use the application.");
+                          startActivity(intent)*/
                 }
-                R.id.logout-> {
+                R.id.logout -> {
                     FirebaseAuth.getInstance().signOut()
-                    Toast.makeText(requireContext(), "Logout Successfully", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(requireContext(), "Logout Successfully", Toast.LENGTH_SHORT)
+                        .show()
                     findNavController().navigate(R.id.action_homeFragment_to_logInFragment)
                 }
             }
@@ -87,13 +92,13 @@ class HomeFragment : Fragment() {
     }
 
     //loading Categories
-    private fun loadCategories(){
+    private fun loadCategories() {
         databaseReference = FirebaseDatabase.getInstance().getReference("categories")
-        databaseReference.addValueEventListener(object : ValueEventListener{
+        databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 categoryList = ArrayList()
-                if (snapshot.exists()){
-                    for (i in snapshot.children){
+                if (snapshot.exists()) {
+                    for (i in snapshot.children) {
                         val category = i.getValue(Category::class.java)
                         Log.d(TAG, "onDataChange: OnDataChanged: $snapshot")
 
@@ -101,15 +106,18 @@ class HomeFragment : Fragment() {
                     }
                     adapter.submitList(categoryList)
                     binding.recyclerView.adapter = adapter
-                }else{
+                } else {
                     Log.d(TAG, "onDataChange: Failed")
-                    Toast.makeText(requireContext(),
-                        "Failed loading data", Toast.LENGTH_SHORT)
+                    Toast.makeText(
+                        requireContext(),
+                        "Failed loading data", Toast.LENGTH_SHORT
+                    )
                         .show()
                 }
             }
+
             override fun onCancelled(error: DatabaseError) {
-                Log.d(TAG, "onCancelled: OnCancelled: "+error.message)
+                Log.d(TAG, "onCancelled: OnCancelled: " + error.message)
                 Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
             }
         })
