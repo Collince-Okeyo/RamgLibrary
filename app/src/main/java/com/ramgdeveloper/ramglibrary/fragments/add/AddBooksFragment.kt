6dcb11eadf5b73c03a94e644d81d6af2aae502da
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-09-29 04:05:01 +0300

    picked pdf from storage

--- app/src/main/java/com/ramgdeveloper/ramglibrary/fragments/add/AddBooksFragment.kt
+++ app/src/main/java/com/ramgdeveloper/ramglibrary/fragments/add/AddBooksFragment.kt
@@ -1,5 +1,6 @@
 package com.ramgdeveloper.ramglibrary.fragments.add
 
+import android.R.attr
 import android.os.Bundle
 import android.text.Editable
 import androidx.fragment.app.Fragment
@@ -11,11 +12,24 @@ import android.widget.ArrayAdapter
 import com.ramgdeveloper.ramglibrary.R
 import com.ramgdeveloper.ramglibrary.databinding.FragmentAddBooksBinding
 import android.content.Intent
+import android.widget.Toast
 
 import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.navigation.fragment.findNavController
+import android.provider.OpenableColumns
 
-private const  val PICK_PDF_CODE = 2
+import android.R.attr.data
+import android.app.Activity.RESULT_OK
+import android.database.Cursor
+import android.net.Uri
+import java.io.File
+import android.app.ProgressDialog
+import android.text.TextUtils
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.ramgdeveloper.ramglibrary.others.Utils
+
+
 class AddBooksFragment : Fragment() {
     private lateinit var binding: FragmentAddBooksBinding
 
@@ -26,13 +40,34 @@ class AddBooksFragment : Fragment() {
     ): View? {
         binding = FragmentAddBooksBinding.inflate(inflater, container, false)
 
-        // Pick TextView
-        binding.pickTV.setOnClickListener {
-            val intentPDF = Intent(Intent.ACTION_GET_CONTENT)
-            intentPDF.type = "application/pdf"
-            intentPDF.addCategory(Intent.CATEGORY_OPENABLE)
-            startActivityForResult(Intent.createChooser(intentPDF, "Select Picture"), PICK_PDF_CODE)
+        // Upload button
+        binding.buttonAdd.setOnClickListener {
+            Utils.hideKeyboard(it)
+
+            if (TextUtils.isEmpty(binding.titleEditText.text.toString())){
+                binding.titleEditText.error = "Title can't be blank"
+            }else if (TextUtils.isEmpty(binding.addDescriptionTV.text.toString())){
+                binding.addDescriptionTV.error = "Please add description"
+            }else if (binding.spinnerCategory.selectedItem.toString() == "Choose Category"){
+                Toast.makeText(requireContext(), "Select Category", Toast.LENGTH_SHORT).show()
+            }else{
+                try {
+                    addBooksToFirebase()
+                } catch (e: Exception){
+                    Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
         }
+
+        // Pick Book
+        binding.pickTV.setOnClickListener(View.OnClickListener {
+            val intent = Intent()
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.type = "application/pdf"
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            startActivityForResult(intent, 1212)
+        })
 
         binding.addBackArrow.setOnClickListener {
             findNavController().navigate(R.id.action_addBooksFragment_to_homeFragment)
@@ -53,11 +88,53 @@ class AddBooksFragment : Fragment() {
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
-                    TODO("Not yet implemented")
+                    Toast.makeText(requireContext(), "Select Category", Toast.LENGTH_SHORT).show()
                 }
             }
         }
 
         return  binding.root
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        when (requestCode) {
+            1212 -> if (resultCode === RESULT_OK) {
+                // Get the Uri of the selected file
+                val uri: Uri = data?.data!!
+                val uriString: String = uri.toString()
+                val myFile: File = File(uriString)
+                val path: String = myFile.name
+                var displayName: String? = null
+                if (uriString.startsWith("content://")) {
+                    var cursor: Cursor? = null
+                    try {
+                        cursor = requireActivity()!!.contentResolver.query(uri, null, null, null, null)
+                        if (cursor != null && cursor.moveToFirst()) {
+                            displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                            binding.pickTV.text = displayName
+                        }
+                    } finally {
+                        cursor?.close()
+                    }
+                } else if (uriString.endsWith("file://")) {
+                    displayName = myFile.name
+                    //binding.pickTV.text = displayName
+                }
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+    private fun addBooksToFirebase(){
+        val pd = ProgressDialog(requireContext())
+        pd.setTitle("Uploading Contents...")
+        pd.setCancelable(false)
+        pd.show()
+
+
+
     }
 }
